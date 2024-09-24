3cac378c0097725f139dd910bd1fe742390aa7d9
gaopengbin <1804287165@qq.com>
2024-09-24 17:11:43 +0800

    first commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+// src/app.ts
+
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, TypeScript and Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
