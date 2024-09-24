0806bc0c253e2fb161893e1866d4e6426b151b52
gaopengbin <1804287165@qq.com>
2024-09-24 17:23:02 +0800

    1

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, TypeScript and Express!');
+  res.send('Hello, TypeScript and Express阿帆反反复复!');
 });
 
 app.listen(PORT, () => {
