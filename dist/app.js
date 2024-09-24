fbf49f68b987aedc2b58e53f512c52b210c06fbc
gaopengbin <1804287165@qq.com>
2024-09-24 21:42:44 +0800

    1

--- dist/app.js
+++ dist/app.js
@@ -5,10 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const sqlite3_1 = __importDefault(require("sqlite3"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-    res.send('Hello, TypeScript and Express阿帆反反复复!');
+const db = new sqlite3_1.default.Database("./gg.db", (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    else {
+        console.log("Connected to the gg database.");
+    }
+});
+app.get("/", (req, res) => {
+    res.send("Hello, TypeScript and Express阿帆反反复复66666!");
 });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,57 @@
 // src/app.ts
 
-import express from 'express';
+import express from "express";
+import sqlite3 from "sqlite3";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, TypeScript and Express阿帆反反复复66666!');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = new sqlite3.Database("./gg.db", (err) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log("Connected to the gg database.");
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello, TypeScript and Express阿帆反反复复66666!");
+});
+
+interface Plan {
+  title: string;
+  cover: string;
+  description: string;
+  content: string;
+}
+app.post("/plan", (req, res) => {
+  const plan: Plan = req.body;
+  console.log(plan);
+  const sql = `INSERT INTO plan (title, cover, description, content) VALUES ('${plan.title}', '${plan.cover}', '${plan.description}', '${plan.content}')`;
+  db.run(sql, (err) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("添加失败");
+    } else {
+      res.status(200).send("添加成功");
+    }
+  });
+});
+
+app.get("/plan", (req, res) => {
+  const sql = "SELECT * FROM plan";
+  db.all(sql, (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("查询失败");
+    } else {
+      res.status(200).send(rows);
+    }
+  });
 });
 
 app.listen(PORT, () => {
